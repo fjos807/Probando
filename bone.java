5321bf442b8e20d20e8f1444714da9257ba76454
Frank Benavides <fjos807@gmail.com>
2017-11-07 22:20:38 -0600

    Solo en master

--- bone.java
+++ bone.java
@@ -3,6 +3,7 @@
 public class Hola {
   public static void main (String args[]){
     System.out.println("Hello");
-    System.out.println("Tratando de hacer un merge")
+    System.out.println("Tratando de hacer un merge");
+    System.out.println("Solamente en master");
   }
 }
