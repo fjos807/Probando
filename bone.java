f2a2dbd85db6be8b4d83192d5d18dcfa94dd569d
Frank Benavides <fjos807@gmail.com>
2017-11-07 22:28:06 -0600

    Tratando de arreglar

--- bone.java
+++ bone.java
@@ -4,6 +4,5 @@ public class Hola {
   public static void main (String args[]){
     System.out.println("Hello");
     System.out.println("Tratando de hacer un merge");
-    System.out.println("Solamente en master");
   }
 }
